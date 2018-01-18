782032557c7dcff7f3e03bf46cffcad473fdd773
Anne Livia <annelivia16@gmail.com>
2018-01-18 17:41:53 -0300

    More submitions

---
+++ 1189/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    double matriz[12][12], soma = 0;
+    int b = 1, cont = 0;
+
+    cin >> c;
+
+    for ( int i = 0 ; i < 12; i++) {
+        for ( int j = 0 ; j < 12; j++) {
+            cin >> matriz[i][j];
+        }
+    }
+
+    for ( int i = 1; i <= 10; i++) {
+        for ( int j = 0; j < b; j++) {
+            soma = soma + matriz[i][j];
+            cont++;
+        }
+        if(i < 5)
+            b++;
+        else if(i >= 6)
+            b--;
+    }
+
+
+    switch (c) {
+        case 'S' : printf("%.1f\n",soma);
+                   break;
+        case 'M' : printf("%.1f\n",soma/cont);
+                   break;
+    }
+    return 0;
+}
