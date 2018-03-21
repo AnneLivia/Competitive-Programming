3855fd9973af3f17f57764cc7aac9cc73b25ec1c
Anne Livia <annelivia16@gmail.com>
2018-03-21 15:14:53 -0300

    New submitions

---
+++ Cobra Coral - OBI2015/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3, n4;
+    cin >> n1 >> n2 >> n3 >> n4;
+    if(n1 == n4)
+        cout << "F\n";
+    else
+        cout << "V\n";
+    return 0;
+}
