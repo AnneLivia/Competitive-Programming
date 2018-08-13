fd3dd437c913a2b2e23230da59baa767d83c4893
Anne Livia <annelivia16@gmail.com>
2018-08-13 15:29:36 -0300

    spoj accepted

---
+++ SPOJ/15711/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "W";
+    for (int i = 0; i < n; i++) {
+        cout << "o";
+    }
+    cout << "w\n";
+    return 0;
+}
