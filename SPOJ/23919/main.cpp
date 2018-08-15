d11cd0138bc1c7c391d690f0176206910207d620
Anne Livia <annelivia16@gmail.com>
2018-08-15 00:00:55 -0300

    spoj new submition

---
+++ SPOJ/23919/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    double n1, n2;
+    cin >> t;
+    while(t--) {
+        cin >> n1 >> n2;
+        cout << n1 + n2 << endl;
+    }
+    return 0;
+}
