e6e6e97161f51aa0debe3a687bf38a8390435701
Anne Livia <annelivia16@gmail.com>
2018-09-13 21:21:39 -0300

    llc

---
+++ Algorithms/Longest Common Subsequence/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    What is the longest common subsequence problem?
+    The longest common subsequence (LCS) problem is the problem of
+    finding the longest subsequence common to all sequences in a set of
+    sequences.
+
+    Applications: DNA Analise of two or more different organisms
+                  A DNA is compound by a sequence of molecules, called bases:
+                  Adenine, Chemin, guanine and cytosine.
+    Given two DNA:
+    S1 = ACCGTGGAAAAGGTTAAGGCCAGGATTTAACCGCGGGC
+    S2 = ACCGCGGTTTAATCCGGATAGGTTGAAATGGTTGAAAC
+
+    Is possible to answer, how much identical are those DNA, and so on.
+
+    For abcd and acd, the answer is LCS(s1, s2) = acd
+
+    Algorithm:
+    Given two strings
+    if s1m == s2n, must do: s1m - 1, s2n - 1 + s1m or s2m.
+    otherwise: check what is the longest, s1m - 1 or s2m - 1.
+
+    c(i,j) = { 0, if i = 0 or j = 0 }
+             { c(i - 1, j - 1) + 1, if i == 1 }
+             { max (c(i - 1, j), c(i, j - 1), if i != j}
+
+    Table:
+    x = BDCABA, Y = ABCBDAB
+        yj  A  B  C  B  D  A  B
+    xi   0  0  0  0  0  0  0  0
+    B    0  0  1  1  1  1  1  1
+    D    0  0  1  1  1  2  2  2
+    C    0  0  1  2  2  2  2  2
+    A    0  1  1  2  2  2  3  3
+    B    0  1  2  2  3  3  3  4
+    A    0  1  2  2  3  3  4  4
+*/
+
+string llc(string s1, string s2) {
+    int xSize = (int)s1.size(); // Getting size of string 1
+    int ySize = (int)s2.size(); // Getting size of string 2
+
+
+    int table[xSize + 1][ySize + 1]; // Creating table of size xSize + 1 and ySize + 1 so that the table is going to have either a free row and col to insert 0
+
+    // Inserting 0 in the first row
+    for(int i = 0; i < ySize + 1; i++) {
+        table[0][i] = 0;
+    }
+
+    // Inserting 0 in the first col
+    for(int i = 0; i < xSize + 1; i++) {
+        table[i][0] = 0;
+    }
+
+    // Filling the table
+    for(int i = 1; i <= xSize; i++) {
+        for(int j = 1; j <= ySize; j++) {
+            if(s1[i - 1] == s2[j - 1]) { // if S1[i] == s2[i], get table[i - 1, j - 1] (element of diagonal) and sum to 1
+                table[i][j] = table[i - 1][j - 1] + 1;
+            } else { // Otherwise, get the maximum, can be either table[i - 1][j] or table[i][j - 1]
+                table[i][j] = table[i - 1][j] > table[i][j - 1] ? table[i - 1][j] : table[i][j - 1];
+            }
+        }
+    }
+
+    // Printing table
+
+    for (int i = 0; i < xSize + 1; i++) {
+        for(int j = 0; j < ySize + 1; j++) {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Finding what is the sequence
+
+    return " ";
+}
+
+int main()
+{
+    llc("ABCB", "BDCAB");
+    return 0;
+}
