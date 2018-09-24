481b82351ea6af8af144c93adc0ef0b1143c1331
Anne Livia <annelivia16@gmail.com>
2018-09-23 21:58:16 -0300

    accepted

--- URI/1367/main.cpp
+++ URI/1367/main.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
+#include <vector>
 #include <map>
-#include <string>
-#include <algorithm>
+
 using namespace std;
 
 int main()
 {
+    int qt, qtA, time, sum;
+    char l;
+    string judge;
+    map<char,int>s;
+    map<char,bool>ok;
+    while(cin >> qt && qt) {
+        qtA = sum = 0;
+        s.clear();
+        ok.clear();
+        while(qt--) {
+            cin >> l >> time >> judge;
+            if(s.find(l) != s.end() && judge != "correct") {
+                s.at(l)+=20;
+            } else if (s.find(l) != s.end() && judge == "correct") {
+                if(!ok.at(l)) {
+                    s.at(l)+=time;
+                    qtA++;
+                }
+                ok.at(l) = true;
 
-    string senha;
-    int num, maius, minusc, espaorsimb;
-    while(getline(cin,senha)) {
-        num = maius = minusc = espaorsimb = 0;
-        for(int i = 0; i < (int)senha.size(); i++) {
-            if(senha[i] == toupper(senha[i]) && isalpha(senha[i])) {
-                maius+=1;
-            } else if(senha[i] == tolower(senha[i]) && isalpha(senha[i])) {
-                minusc+=1;
-            } else if((senha[i] - '0') >= 0 && (senha[i] - '0') <= 9) {
-                num+=1;
             } else {
-                espaorsimb+=1;
+                if(judge == "correct") {
+                    s.insert(make_pair(l,time));
+                    qtA++;
+                    ok.insert(make_pair(l,true));
+                } else {
+                    s.insert(make_pair(l,20));
+                    ok.insert(make_pair(l,false));
+                }
             }
-
+        }
+        if(qtA != 0) {
+            for(auto it = s.begin(); it != s.end(); it++) {
+                if(ok.at(it->first))
+                    sum+=it->second;
+            }
         }
 
-        if(maius >= 1 && minusc >= 1 && num >= 1 && espaorsimb == 0 && (int)senha.size() >= 6 && (int)senha.size() <= 32)
-            cout << "Senha valida.\n";
-        else
-            cout << "Senha invalida.\n";
+        cout << qtA << " " << sum << endl;
     }
     return 0;
 }
