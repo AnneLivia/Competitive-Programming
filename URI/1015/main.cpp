f1a37274727793fb0d794e5961c061ed04032466
Anne Livia <annelivia16@gmail.com>
2018-10-16 12:48:24 -0300

    update

--- URI/1015/main.cpp
+++ URI/1015/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <math.h>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+
+    double x1,y1,x2,y2;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+
+    printf("%.4f\n",sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
     return 0;
 }
