f9200d51ac9fd4113cc011c958262c08ba891e0c
Anne Livia <annelivia16@gmail.com>
2019-05-21 00:05:12 -0300

    accepted

--- Online Judges/URI/Calculo (OBI)/main.cpp
+++ Online Judges/URI/Calculo (OBI)/main.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-// Falta terminar
 
 ostream& operator <<(ostream& ostr, const vector<int>& v) {
     for(int i = 0; i < (int)v.size(); i++)
@@ -13,12 +12,15 @@ ostream& operator <<(ostream& ostr, const vector<int>& v) {
 
 int main()
 {
-    vector<int> n1, n2, n3, nibble, aux1,aux2;
+
     int tam1, tam2, n;
-    cin >> tam1 >> tam2; // Reading the size of the arrays
+    vector<int>n1, n2, n3, aux1, aux2, nibble;
+
+    cin >> tam1 >> tam2;
+
     for ( int i = 0; i < tam1; i++) {
         cin >> n;
-        n1.push_back(n); // Putting the values in the array 1
+        n1.push_back(n);
     }
 
     for ( int i = 0; i < tam2; i++) {
@@ -38,6 +40,7 @@ int main()
         }
         tam2 = tam1;
     }
+
     // The above loop is to invert the number typed, because of the operation that occurs from the right to left.
     for ( int i = tam1 - 1; i >= 0; i--) {
         aux1.push_back(n1[i]);
@@ -46,41 +49,43 @@ int main()
 
     n1 = aux1;
     n2 = aux2;
+
     aux1.clear(); aux2.clear();
+
+    n3.assign(tam1, 0);
     // The operation is running bellow
     nibble.assign(tam1 + 1, 0);
-    for ( int i = 0; i < tam1; i++) {
-        if(n1[i] == 0 && n2[i] == 1 || n1[i] == 1 && n2[i] == 0 && nibble[i] == 0)
-            n3.push_back(1);
-        else if (n1[i] == 0 && n2[i] == 1 || n1[i] == 1 && n2[i] == 0 && nibble[i] == 1) {
-            nibble[i + 1] = 1;
-            n3.push_back(0);
-        } else if (n1[i] == 0 && n2[i] == 0 && nibble[i] == 0)
-            n3.push_back(0);
-        else if (n1[i] == 1 && n2[i] == 1 && nibble[i] == 0) {
-            n3.push_back(0);
-            nibble[i + 1] = 1;
-        } else if (n1[i] == 1 && n2[i] == 1 || n1[i] == 0 && n2[i] == 0 && nibble[i] == 1){
-            n3.push_back(1);
-            if(n1[i] == 1)
+
+    for (int i = 0; i < tam1; i++) {
+        if((n1[i] == 1 && n2[i] == 0) || (n1[i] == 0 && n2[i] == 1)) {
+            if(nibble[i] != 1) {
+                n3[i] = 1;
+            } else {
                 nibble[i + 1] = 1;
+            }
+        } else if (n1[i] == 0 && n2[i] == 0) {
+            if(nibble[i] == 1)
+                n3[i] = 1;
+        } else {
+            if(nibble[i] == 1) {
+                n3[i] = 1;
+            }
+            nibble[i + 1] = 1;
         }
     }
 
 
-    bool flag = true;
-    for ( int i = 0; i < tam1; i++) {
-        if(n3[i] == 1 && flag) {
-            flag = false;
-        }
-        if(!flag)
-            aux2.push_back(n3[i]);
-    }
-    // Inverting the n3, since is in the inverse order
-    for ( int i = (int)aux2.size() - 1; i >= 0; i--) {
-        aux1.push_back(aux2[i]);
+
+    int i;
+    for ( i = 0; i < (int)n3.size(); i++) {
+        if(n3[i] != 0)
+            break;
     }
 
-    cout << aux1 << endl;
+    for (int j = (int)n3.size() - 1; j >= i; j--) {
+        aux1.push_back(n3[j]);
+    }
+
+    cout << aux1 <<  endl;
     return 0;
 }
