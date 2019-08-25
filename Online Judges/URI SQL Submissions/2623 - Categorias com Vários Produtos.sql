d4c7e8c25d020e1f0c3045878f58d590af9cf0e0
Anne Livia <annelivia16@gmail.com>
2019-08-24 21:09:19 -0300

    new sql

---
+++ Online Judges/URI SQL Submissions/2623 - Categorias com Vários Produtos.sql
@@ -0,0 +1,47 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 2623
+
+CREATE TABLE categories (
+  id numeric PRIMARY KEY,
+  name varchar(255)
+);
+
+CREATE TABLE products (
+  id numeric PRIMARY KEY,
+  name varchar (255),
+  amount numeric,
+  price numeric,
+  id_categories numeric REFERENCES categories (id)
+);
+
+
+INSERT INTO categories (id, name)
+VALUES
+  (1,	'Superior'),
+  (2,	'Super Luxury'),
+  (3,	'Modern'),
+  (4,	'Nerd'),
+  (5,	'Infantile'),
+  (6,	'Robust'),
+  (9,	'Wood');
+
+INSERT INTO products (id, name, amount, price, id_categories)
+VALUES
+  (1,	'Blue Chair',	30, 300.00,	9),
+  (2,	'Red Chair',	200,	2150.00, 2),
+  (3,	'Disney Wardrobe',	400,	829.50,	4),
+  (4,	'Blue Toaster',	20,	9.90,	3),
+  (5,	'Solar Panel',	30,	3000.25,	4);
+
+SELECT products.name, categories.name 
+FROM products 
+JOIN categories 
+ON (products.id_categories = categories.id)  
+WHERE 
+((products.id_categories >= 1 AND products.id_categories <= 3)
+or (products.id_categories = 6 or products.id_categories = 9)) 
+and products.amount > 100 ORDER BY categories.id;
+/*  Execute this query to drop the tables */
+DROP TABLE products, categories; --
