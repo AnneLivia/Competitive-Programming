50db6816eb39f6b44a13f07009dcebcb65fd0250
Anne Livia <annelivia16@gmail.com>
2019-08-29 09:40:48 -0300

    accepted sql

---
+++ Online Judges/URI SQL Submissions/2624 - Quantidade de Cidades por Clientes.sql
@@ -0,0 +1,28 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 2624
+
+CREATE TABLE customers (
+  id numeric PRIMARY KEY,
+  name varchar(255),
+  street varchar(255),
+  city varchar(255),
+  state char(2),
+  credit_limit numeric
+);
+
+
+INSERT INTO customers (id, name, street, city, state, credit_limit)
+VALUES
+  (1,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (2,	'Cecília Olivia Rodrigues', 'Rua Sizuka Usuy',	'Cianorte',	'PR',	3170),
+  (3,	'Augusto Fernando Carlos Eduardo Cardoso',	'Rua Baldomiro Koerich', 'Palhoça',	'SC',	1067),
+  (4,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (5,	'Sabrina Heloisa Gabriela Barros',	'Rua Engenheiro Tito Marques Fernandes',	'Porto Alegre',	'RS',	4312),
+  (6,	'Joaquim Diego Lorenzo Araújo',	'Rua Vitorino',	'Novo Hamburgo',	'RS',	2314);
+  
+  select count(distinct city) as count from customers;
+
+  /*  Execute this query to drop the tables */
+  DROP TABLE customers; -- 
