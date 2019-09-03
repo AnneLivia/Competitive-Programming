ae1928cfebbc642a1fd3809495bc68dbd3fce59e
Anne Livia <annelivia16@gmail.com>
2019-09-03 16:33:54 -0300

    accepted

---
+++ Online Judges/URI SQL Submissions/2740 - Liga.sql
@@ -0,0 +1,35 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 2740
+
+CREATE TABLE league(
+  position INTEGER,
+  team CHARACTER VARYING (255)
+);
+
+
+INSERT INTO league(position, team)
+VALUES (1, 'The Quack Bats'),
+       (2, 'The Responsible Hornets'),
+       (3, 'The Bawdy Dolphins'),
+       (4, 'The Abstracted Sharks'),
+       (5, 'The Nervous Zebras'),
+       (6, 'The Oafish Owls'),
+       (7, 'The Unequaled Bison'),
+       (8, 'The Keen Kangaroos'),
+       (9, 'The Left Nightingales'),
+       (10, 'The Terrific Elks'),
+       (11, 'The Lumpy Frogs'),
+       (12, 'The Swift Buffalo'),
+       (13, 'The Big Chargers'),
+       (14, 'The Rough Robins'),
+       (15, 'The Silver Crocs');
+
+  (SELECT concat('Podium: ', team) AS name FROM league ORDER BY position LIMIT 3 )
+  UNION ALL
+  (SELECT concat('Demoted: ', team) FROM league 
+  WHERE position >= cast((select count(*) from league limit 1) AS int) - 1);
+  
+  /*  Execute this query to drop the tables */
+  DROP TABLE league; --
