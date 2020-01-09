7f783467cae97671960bc773fda6aefffe1f545e
Anne Livia <annelivia16@gmail.com>
2020-01-09 19:24:02 -0300

    accepted

---
+++ Online Judges/CodeForces/J. Eleições (IFB)/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, n, t = 0, vv = 0, mini = -1;
+    cin >> a >> b >> n;
+
+    cout << ((int)n/a) * b << endl;
+    return 0;
+}
