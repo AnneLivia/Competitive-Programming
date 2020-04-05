1875212cfdc4b762659ac32fef152daded8eec37
Anne Livia <annelivia16@gmail.com>
2020-04-05 19:43:52 -0300

    accepted

---
+++ Online Judges/Neps Academy/Dez Valores/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    for (int i = x + 1; i <= x + 10; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
