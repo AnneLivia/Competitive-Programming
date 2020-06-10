9fdbbae6c2ecffc9ad742b7d217bc9a34255d300
Anne Livia <annelivia16@gmail.com>
2020-06-10 12:52:08 -0300

    accepted

---
+++ Online Judges/Neps Academy/Notas da Prova (OBI 2009)/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 0)
+        cout << "E\n";
+    else if (n >= 1 && n <= 35) {
+        cout << "D\n";
+    } else if (n >= 36 && n <= 60) {
+        cout << "C\n";
+    } else if (n >= 61 && n <= 85) {
+        cout << "B\n";
+    } else {
+        cout << "A\n";
+    }
+    return 0;
+}
