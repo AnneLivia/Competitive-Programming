a234543c0a774a374bdb21ec3acadbe7ec4d9051
Anne Livia <annelivia16@gmail.com>
2020-08-05 19:41:46 -0300

    accepted

---
+++ Online Judges/Neps Academy/AlbumdeFotosOBI2012.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+struct album {
+    int lar, alt;
+};
+
+// try change
+int main()
+{
+    album al, ph1, ph2;
+
+    cin >> al.lar >> al.alt;
+    cin >> ph1.lar >> ph1.alt;
+    cin >> ph2.lar >> ph2.alt;
+
+    if(ph1.lar + ph2.lar <= al.lar && ph1.alt <= al.alt && ph2.alt <= al.alt) {
+        cout << "S\n";
+    } else if (ph1.alt + ph2.alt <= al.alt && ph1.lar <= al.lar && ph2.lar <= al.lar) {
+        cout << "S\n";
+    } else if (ph1.alt + ph2.lar <= al.alt && ph2.alt <= al.lar && ph1.lar <= al.lar) {
+        cout << "S\n";
+    } else if (ph1.lar + ph2.alt <= al.alt && ph1.alt <= al.lar && ph2.lar <= al.lar) {
+        cout << "S\n";
+    } else if(ph1.lar + ph2.lar <= al.alt && ph1.alt <= al.lar && ph2.alt <= al.lar) {
+        cout << "S\n";
+    } else if (ph1.alt + ph2.alt <= al.lar && ph1.lar <= al.alt && ph2.lar <= al.alt) {
+        cout << "S\n";
+    } else if (ph1.alt + ph2.lar <= al.lar && ph2.alt <= al.alt && ph1.lar <= al.alt) {
+        cout << "S\n";
+    } else if (ph1.lar + ph2.alt <= al.lar && ph1.alt <= al.alt && ph2.lar <= al.alt) {
+        cout << "S\n";
+    } else {
+        cout << "N\n";
+    }
+
+
+    return 0;
+}
