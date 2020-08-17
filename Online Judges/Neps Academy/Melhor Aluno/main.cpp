22baacd9f27df237224307288bc10a48eed6250a
Anne Livia <annelivia16@gmail.com>
2020-08-17 18:55:43 -0300

    accepted

---
+++ Online Judges/Neps Academy/Melhor Aluno/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    if (a < b)
+        cout << "Pedro\n";
+    else if (a > b) {
+        cout << "Paulo\n";
+    } else {
+        cout << "Pedro\n";
+    }
+    return 0;
+}
