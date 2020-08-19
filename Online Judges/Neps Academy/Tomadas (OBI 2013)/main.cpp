1277883d3b96481b10e44c4578a577461ecdaa28
Anne Livia <annelivia16@gmail.com>
2020-08-19 17:46:19 -0300

    accepted

---
+++ Online Judges/Neps Academy/Tomadas (OBI 2013)/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3, n4;
+    cin >> n1 >> n2 >> n3 >> n4;
+    cout << (n1 - 1) + (n2 - 1) + (n3 - 1) + n4 << endl;
+    return 0;
+}
