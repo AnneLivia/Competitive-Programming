2000587322079a43bb31c0501f5b7fdd867d5b75
Anne Livia <annelivia16@gmail.com>
2020-10-08 18:15:25 -0300

    accepted

---
+++ Online Judges/Neps Academy/PedagioOBI2002.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int> > g;
+
+void bfs(int l, int p) {
+    int sz = (int)g.size();
+    vector<int>dist(sz, -1);
+    queue<int>q;
+    q.push(l);
+    dist[l] = 0;
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int i = 0; i < (int)g[u].size(); i++)
+        {
+            int uu = g[u][i];
+            if (dist[uu] == -1) {
+                dist[uu] = dist[u] + 1;
+                q.push(uu);
+            }
+        }
+    }
+
+    int space = 0;
+    for (int i = 0; i < sz; i++)
+    {
+        if (i != l) {
+            if (dist[i] <= p && dist[i] != -1) {
+                if (space)
+                    cout << " "; 
+                cout << i + 1;
+                space = 1;
+            }
+        }
+    }
+}
+
+int main() {
+    int c, e, l, p, a, b, t = 1;
+    while(cin >> c >> e >> l >> p && c + e + l + p != 0) {
+        g.assign(c, vector<int>());
+        while(e--) {
+            cin >> a >> b;
+            --a; --b;
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+        cout << "Teste " << t++ << endl;
+        --l;
+        bfs(l, p);
+        cout << "\n\n";
+    }
+    return 0;  
+}
