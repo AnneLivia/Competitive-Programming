02ff972be172a62b8abe25030c3676f6c04efd1b
Anne Livia <annelivia16@gmail.com>
2022-01-25 16:58:38 -0300

    new problem from uri

---
+++ Online Judges/URI/2437.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x1, x2, y1, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    cout << abs(x2 - x1) + abs(y2 - y1) << endl;
+    return 0;
+}
